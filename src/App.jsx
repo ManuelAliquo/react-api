9dd899997334003c870ae236f2cbf1b40e558328
ManuelAliquo <manuelaliquo2103@gmail.com>
2026-02-13 17:04:24 +0100

    milestone 2: designed basic card

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
+
+const baseUrl = "https://lanciweb.github.io";
+const actorsUrl = "/demo/api/actors/";
 
 export default function App() {
-  const baseUrl = "https://lanciweb.github.io";
-  axios.get(baseUrl + "/demo/api/actors/").then((res) => {
-    console.log(res.data);
-  });
+  const [actors, setActors] = useState([]);
+
+  useEffect(() => {
+    axios.get(baseUrl + actorsUrl).then((res) => {
+      setActors(res.data);
+    });
+  }, []);
+
   return (
     <>
-      <h1>Hello</h1>
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-2">
+            <div className="card">
+              <img className="card-img-top" src="..." alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Nome</h5>
+                <div className="birth">Anno di nascita</div>
+                <div className="nationality">Nazionalità</div>
+                <p className="bio mb-0">Biografia</p>
+                <div className="awards">Riconoscimenti</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
