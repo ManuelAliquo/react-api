adc697ba0b870cd08733e9d6b519a5b3b1f4cafc
ManuelAliquo <manuelaliquo2103@gmail.com>
2026-02-13 17:29:47 +0100

    milestone 3: generated actors card

--- src/App.jsx
+++ src/App.jsx
@@ -15,20 +15,36 @@ export default function App() {
 
   return (
     <>
-      <div className="container mt-5">
-        <div className="row">
-          <div className="col-2">
-            <div className="card">
-              <img className="card-img-top" src="..." alt="..." />
-              <div className="card-body">
-                <h5 className="card-title">Nome</h5>
-                <div className="birth">Anno di nascita</div>
-                <div className="nationality">Nazionalità</div>
-                <p className="bio mb-0">Biografia</p>
-                <div className="awards">Riconoscimenti</div>
+      <div className="container my-4">
+        <h1 className="text-center mb-4">Actors List</h1>
+        <div className="row row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-lg-6 g-2">
+          {actors.map((actor) => {
+            const { image, name, birth_year, nationality, biography, awards } = actor;
+            return (
+              <div key={actor.id} className="col">
+                <div className="card">
+                  <img className="card-img-top" src={image} alt={name} />
+                  <div className="card-body">
+                    <h5 className="card-title mb-0">{name}</h5>
+                    <div className="birth-nationality border-bottom mb-1">
+                      {birth_year} - {nationality}
+                    </div>
+                    <p className="bio mb-1">{biography}</p>
+                    <ul className="awards list-unstyled border-top mb-0">
+                      Awards:{" "}
+                      {awards.map((award, index) => {
+                        return (
+                          <li key={index}>
+                            <strong>{award}</strong>
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
+            );
+          })}
         </div>
       </div>
     </>
