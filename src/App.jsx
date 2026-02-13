f95cf0a0895f6c1930359d01fea59daf18efa981
ManuelAliquo <manuelaliquo2103@gmail.com>
2026-02-13 19:32:08 +0100

    bonus 1: rendered actresses list and actors list separately

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,38 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
+// Components
+import ActorsList from "./components/ActorsList";
+import ActressesList from "./components/ActressesList";
+
+// URL
 const baseUrl = "https://lanciweb.github.io";
-const actorsUrl = "/demo/api/actors/";
+const actorsEndpoint = "/demo/api/actors/";
+const actressesEndpoint = "/demo/api/actresses/";
 
 export default function App() {
   const [actors, setActors] = useState([]);
+  const [actresses, setActresses] = useState([]);
 
   useEffect(() => {
-    axios.get(baseUrl + actorsUrl).then((res) => {
-      setActors(res.data);
-    });
+    axios.get(baseUrl + actressesEndpoint).then((res) => setActresses(res.data));
+    axios.get(baseUrl + actorsEndpoint).then((res) => setActors(res.data));
   }, []);
 
   return (
     <>
       <div className="container my-4">
-        <h1 className="text-center mb-4">Actors List</h1>
-        <div className="row row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-lg-6 g-2">
-          {actors.map((actor) => {
-            const { image, name, birth_year, nationality, biography, awards } = actor;
-            return (
-              <div key={actor.id} className="col">
-                <div className="card">
-                  <img className="card-img-top" src={image} alt={name} />
-                  <div className="card-body">
-                    <h5 className="card-title mb-0">{name}</h5>
-                    <div className="birth-nationality border-bottom mb-1">
-                      {birth_year} - {nationality}
-                    </div>
-                    <p className="bio mb-1">{biography}</p>
-                    <ul className="awards list-unstyled border-top mb-0">
-                      Awards:{" "}
-                      {awards.map((award, index) => {
-                        return (
-                          <li key={index}>
-                            <strong>{award}</strong>
-                          </li>
-                        );
-                      })}
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            );
-          })}
-        </div>
+        <h1 className="text-center mb-4">Actresses & Actors</h1>
+        {/* Actresses */}
+        <section>
+          <h2 className="mt-4 ms-2">Actresses List</h2>
+          <ActressesList actresses={actresses} />
+        </section>
+        {/* Actors */}
+        <section>
+          <h2 className="mt-4 ms-2">Actors List</h2>
+          <ActorsList actors={actors} />
+        </section>
       </div>
     </>
   );
---
+++ src/components/ActorsList.jsx
@@ -0,0 +1,33 @@
+export default function ActorsList({ actors }) {
+  return (
+    <div className="row row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-lg-6 g-2">
+      {actors.map((actor) => {
+        const { image, name, birth_year, nationality, biography, awards } = actor;
+        return (
+          <div key={actor.id} className="col">
+            <div className="card">
+              <img className="card-img-top" src={image} alt={name} />
+              <div className="card-body">
+                <h5 className="card-title mb-0">{name}</h5>
+                <div className="birth-nationality border-bottom mb-1">
+                  {birth_year} - {nationality}
+                </div>
+                <p className="bio mb-1">{biography}</p>
+                <div className="list-heading mt-2 border-top">Awards:</div>
+                <ul className="awards list-unstyled mb-0">
+                  {awards.map((award, index) => {
+                    return (
+                      <li key={index}>
+                        <strong>{award}</strong>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
