733f6bc27c993dd390628771b2499c0d973616e4
ManuelAliquo <manuelaliquo2103@gmail.com>
2026-02-13 16:14:25 +0100

    milestone 1: actors list printed in console

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import axios from "axios";
 
 export default function App() {
+  const baseUrl = "https://lanciweb.github.io";
+  axios.get(baseUrl + "/demo/api/actors/").then((res) => {
+    console.log(res.data);
+  });
   return (
     <>
       <h1>Hello</h1>
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
@@ -6,8 +5,4 @@ import "bootstrap/dist/js/bootstrap.min.js";
 import "./assets/css/index.css";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+createRoot(document.getElementById("root")).render(<App />);
